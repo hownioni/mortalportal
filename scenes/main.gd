27938bbae13c402046a69136fa762ed14bc8ef24
Hownioni <honihoneyhowni@gmail.com>
2026-03-04 15:20:14 -0600

    Juego funcional para presentar

---
+++ scenes/main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var levels: Array[PackedScene]
+@export var sub_viewport: SubViewport
+var player: CharacterBody2D = null
+
+var _curr_lvl := 1
+var _insted_lvl: Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    var dict: Dictionary = SaveManager.load_game()
+    _create_lvl(_curr_lvl)
+
+func _create_lvl(lvl_num: int) -> void:
+    _insted_lvl = levels[lvl_num - 1].instantiate()
+    sub_viewport.add_child(_insted_lvl)
+    player = _insted_lvl.get_node("Player")
