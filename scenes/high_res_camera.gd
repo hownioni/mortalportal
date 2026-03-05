138ae708d47af4a2017c4b37b12280843f9f1d91
Hownioni <honihoneyhowni@gmail.com>
2026-03-04 20:22:51 -0600

    Nose pero de ahuevo quiere que le haga commit ptm

--- scenes/high_res_camera.gd
+++ scenes/high_res_camera.gd
@@ -6,16 +6,16 @@ extends Camera2D
 @export var velocity_influence := 0.2
 
 func _physics_process(delta: float) -> void:
-    var player: CharacterBody2D = get_parent().player
-    var center_pos := sub_viewport_container.global_position + (sub_viewport_container.size / 2)
-    
-    var target_offset := player.velocity * velocity_influence
-    
-    var max_offset := 50.0
-    target_offset.x = clamp(target_offset.x, -max_offset, max_offset)
-    target_offset.y = clamp(target_offset.y, -max_offset, max_offset)
-    
-    var target_pos = center_pos + target_offset
-    
-    global_position = global_position.lerp(target_pos, smooth_speed * delta)
-    
+	var player: CharacterBody2D = get_parent().player
+	var center_pos := sub_viewport_container.global_position + (sub_viewport_container.size / 2)
+	
+	var target_offset := player.velocity * velocity_influence
+	
+	var max_offset := 50.0
+	target_offset.x = clamp(target_offset.x, -max_offset, max_offset)
+	target_offset.y = clamp(target_offset.y, -max_offset, max_offset)
+	
+	var target_pos = center_pos + target_offset
+	
+	global_position = global_position.lerp(target_pos, smooth_speed * delta)
+	
--- singleton/scene_manager.gd
+++ singleton/scene_manager.gd
@@ -3,8 +3,8 @@ extends Node
 signal game_paused
 
 func pause_game(is_paused: bool) -> void:
-    get_tree().paused = is_paused
-    
-    if is_paused:
-        game_paused.emit()
-    
+	get_tree().paused = is_paused
+	
+	if is_paused:
+		game_paused.emit()
+	
