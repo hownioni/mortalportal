8167da5cb7a06659140bed83fa6e1235ad83c9fc
Hownioni <honihoneyhowni@gmail.com>
2026-03-04 20:28:29 -0600

    Revert "Nose pero de ahuevo quiere que le haga commit ptm"
    This reverts commit 776d82b4f25d3317588da63611098da5e0eae282.

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,10 +9,10 @@ var _insted_lvl: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dict: Dictionary = SaveManager.load_game()
-	_create_lvl(_curr_lvl)
+    var dict: Dictionary = SaveManager.load_game()
+    _create_lvl(_curr_lvl)
 
 func _create_lvl(lvl_num: int) -> void:
-	_insted_lvl = levels[lvl_num - 1].instantiate()
-	sub_viewport.add_child(_insted_lvl)
-	player = _insted_lvl.get_node("Player")
+    _insted_lvl = levels[lvl_num - 1].instantiate()
+    sub_viewport.add_child(_insted_lvl)
+    player = _insted_lvl.get_node("Player")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,32 +8,32 @@ const JUMP_VELOCITY := -350.0
 @export var pause_menu: PauseMenu
 
 func _ready() -> void:
-	add_to_group("Player")
+    add_to_group("Player")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
-		SceneManager.pause_game(true)
+    if event.is_action_pressed("pause"):
+        SceneManager.pause_game(true)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+    # Add the gravity.
+    if not is_on_floor():
+        velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+    # Handle jump.
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-		if direction < 0:
-			animated_sprite_2d.play("idle_left")
-		else:
-			animated_sprite_2d.play("idle_right")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+    # Get the input direction and handle the movement/deceleration.
+    var direction := Input.get_axis("left", "right")
+    if direction:
+        velocity.x = direction * SPEED
+        if direction < 0:
+            animated_sprite_2d.play("idle_left")
+        else:
+            animated_sprite_2d.play("idle_right")
+    else:
+        velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
-	global_position = global_position.round()
+    move_and_slide()
+    
+    global_position = global_position.round()
