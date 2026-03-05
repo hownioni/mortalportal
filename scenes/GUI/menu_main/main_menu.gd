776d82b4f25d3317588da63611098da5e0eae282
Hownioni <honihoneyhowni@gmail.com>
2026-03-04 20:22:51 -0600

    Nose pero de ahuevo quiere que le haga commit ptm

--- scenes/GUI/menu_main/main_menu.gd
+++ scenes/GUI/menu_main/main_menu.gd
@@ -3,14 +3,14 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    get_tree().paused = true
+	get_tree().paused = true
 
 func _on_start_button_pressed() -> void:
-    get_tree().paused = false
-    get_tree().change_scene_to_file("res://scenes/GUI/menu_saves/saves_menu.tscn")
-    
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/GUI/menu_saves/saves_menu.tscn")
+	
 func _on_options_button_pressed() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _on_exit_button_pressed() -> void:
-    get_tree().quit()
+	get_tree().quit()
--- singleton/save_manager.gd
+++ singleton/save_manager.gd
@@ -4,32 +4,32 @@ const save_file_name: String = "user://save.jsons"
 const default_dictionary: Dictionary = {"level": 1, "time_elapsed": 0.0}
 
 func save_game(data: Dictionary) -> void:
-    var save_file: FileAccess = FileAccess.open(save_file_name, FileAccess.WRITE)
-    if save_file == null:
-        push_error("Error opening file")
-        return
-    
-    var string_data: String = JSON.stringify(data)
-    save_file.store_line(string_data)
-    save_file.close()
-    
+	var save_file: FileAccess = FileAccess.open(save_file_name, FileAccess.WRITE)
+	if save_file == null:
+		push_error("Error opening file")
+		return
+	
+	var string_data: String = JSON.stringify(data)
+	save_file.store_line(string_data)
+	save_file.close()
+	
 func load_game() -> Dictionary:
-    if FileAccess.file_exists(save_file_name):
-        var save_file: FileAccess = FileAccess.open(save_file_name, FileAccess.READ)
-        if save_file == null:
-            push_error("Error reading file")
-            return default_dictionary
-        
-        var json = JSON.new()
-        
-        var string_data: String = save_file.get_line()
-        if json.parse(string_data) == OK:
-            var data: Dictionary = json.get_data()
-            save_file.close()
-            return data
-        push_error("Corrupted data")
+	if FileAccess.file_exists(save_file_name):
+		var save_file: FileAccess = FileAccess.open(save_file_name, FileAccess.READ)
+		if save_file == null:
+			push_error("Error reading file")
+			return default_dictionary
+		
+		var json = JSON.new()
+		
+		var string_data: String = save_file.get_line()
+		if json.parse(string_data) == OK:
+			var data: Dictionary = json.get_data()
+			save_file.close()
+			return data
+		push_error("Corrupted data")
 
-    return default_dictionary
+	return default_dictionary
 
 func reset_game() -> void:
-    save_game(default_dictionary)
+	save_game(default_dictionary)
