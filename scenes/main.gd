44d6b1551a6b666c804b2ae4b9284fba0e5cfb26
Hownioni <honihoneyhowni@gmail.com>
2026-03-18 08:05:27 -0600

    Added killzone and fixed camera

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @export var levels: Array[PackedScene]
 @export var sub_viewport: SubViewport
-var player: CharacterBody2D = null
+var player: Player = null
+var low_res_camera: Camera2D = null
 
 var _curr_lvl := 1
 var _insted_lvl: Node
@@ -15,3 +16,12 @@ func _create_lvl(lvl_num: int) -> void:
     _insted_lvl = levels[lvl_num - 1].instantiate()
     sub_viewport.add_child(_insted_lvl)
     player = _insted_lvl.get_node("Player")
+    #low_res_camera = _insted_lvl.get_node("LowResCamera")
+    player.player_died.connect(_reset_lvl)
+
+func _remove_lvl() -> void:
+    _insted_lvl.queue_free()
+
+func _reset_lvl() -> void:
+    _remove_lvl()
+    _create_lvl(_curr_lvl)
