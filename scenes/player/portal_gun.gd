f532166caa5a0e19b6bfa717719bada1ebcd64de
Hownioni <honihoneyhowni@gmail.com>
2026-04-15 09:51:50 -0600

    Moví unas cosillas

--- scenes/player/portal_gun.gd
+++ scenes/player/portal_gun.gd
@@ -4,9 +4,10 @@ extends Sprite2D
 
 var player: Player
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
     player = owner.player
     var gun_pivot: Node2D = player.get_node("GunPivot")
     global_position = gun_pivot.position
+
     look_at(get_global_mouse_position())
     flip_v = player.animated_sprite_2d.flip_h
