4b4d3b3106853debee8274161200cf06e17366ab
Hownioni <honihoneyhowni@gmail.com>
2026-05-27 07:00:11 -0600

    Align PortalArm to GunPivot offset in main scene
    Scale gun_pivot.position by SubViewport->SubViewportContainer ratio
    so the arm tracks the exact pivot point regardless of where it was
    placed within the player scene.

--- characters/player/portal_arm.gd
+++ characters/player/portal_arm.gd
@@ -2,11 +2,14 @@ class_name PortalArm extends Node2D
 
 @export var player: Player
 @export var sub_viewport_container: SubViewportContainer
+@export var sub_viewport: SubViewport
 
 @onready var _sprite: Sprite2D = $Sprite2D
 
 
 func _process(_delta: float) -> void:
-    global_position = sub_viewport_container.global_position + Vector2(sub_viewport_container.size) / 2.0
+    var scale := Vector2(sub_viewport_container.size) / Vector2(sub_viewport.size)
+    var center := sub_viewport_container.global_position + Vector2(sub_viewport_container.size) / 2.0
+    global_position = center + player.gun_pivot.position * scale
     rotation = player.gun_rotation
     _sprite.flip_v = rotation > PI / 2.0 or rotation < -PI / 2.0
