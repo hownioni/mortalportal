004aa3603cb0da591b8b6086aeeebb58cffc115b
Hownioni <honihoneyhowni@gmail.com>
2026-05-27 06:54:08 -0600

    Fix PortalArm position using SubViewportContainer center
    player.global_position is in SubViewport space (640x360), not main
    scene space. Mirror the HighResCamera approach: place the arm at
    sub_viewport_container center, where the player is always visually.

--- characters/player/portal_arm.gd
+++ characters/player/portal_arm.gd
@@ -1,11 +1,12 @@
 class_name PortalArm extends Node2D
 
 @export var player: Player
+@export var sub_viewport_container: SubViewportContainer
 
 @onready var _sprite: Sprite2D = $Sprite2D
 
 
 func _process(_delta: float) -> void:
-	global_position = player.global_position
-	rotation = player.gun_rotation
-	_sprite.flip_v = rotation > PI / 2.0 or rotation < -PI / 2.0
+    global_position = sub_viewport_container.global_position + Vector2(sub_viewport_container.size) / 2.0
+    rotation = player.gun_rotation
+    _sprite.flip_v = rotation > PI / 2.0 or rotation < -PI / 2.0
