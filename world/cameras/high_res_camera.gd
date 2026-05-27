524982800306dc7953d42ca071eb3da2e72af5ca
Hownioni <honihoneyhowni@gmail.com>
2026-05-27 04:00:22 -0600

    Actualicé la chingadera de la cámara

--- world/cameras/high_res_camera.gd
+++ world/cameras/high_res_camera.gd
@@ -8,8 +8,8 @@ class_name HighResCamera extends Camera2D
 const MAX_OFFSET := 50.0
 
 func _physics_process(delta: float) -> void:
-    var center_pos := sub_viewport_container.global_position + Vector2(sub_viewport_container.size) / 2.0
-    var target_offset := player.velocity * velocity_influence
-    target_offset.x = clamp(target_offset.x, -MAX_OFFSET, MAX_OFFSET)
-    target_offset.y = clamp(target_offset.y, -MAX_OFFSET, MAX_OFFSET)
-    global_position = global_position.lerp(center_pos + target_offset, smooth_speed * delta)
+	var center_pos := sub_viewport_container.global_position + Vector2(sub_viewport_container.size) / 2.0
+	var target_offset := player.velocity * velocity_influence
+	target_offset.x = clamp(target_offset.x, -MAX_OFFSET, MAX_OFFSET)
+	target_offset.y = clamp(target_offset.y, -MAX_OFFSET, MAX_OFFSET)
+	global_position = global_position.lerp(center_pos + target_offset, smooth_speed * delta)
