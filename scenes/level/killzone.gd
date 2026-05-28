fc819236f785a68534c8a3805bf4afcfa303b4dd
Hownioni <honihoneyhowni@gmail.com>
2026-05-27 18:08:03 -0600

    Mátenme

--- scenes/level/killzone.gd
+++ scenes/level/killzone.gd
@@ -3,8 +3,8 @@ extends Area2D
 @onready var world_bound: CollisionShape2D = $WorldBound
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		var player: Player = body
-		# Usamos call_deferred para retrasar la muerte una fracción de frame
-		# y permitir que la física termine de procesar la colisión limpiamente.
-		player.call_deferred("die")
+    if body is Player:
+        var player: Player = body
+        # Usamos call_deferred para retrasar la muerte una fracción de frame
+        # y permitir que la física termine de procesar la colisión limpiamente.
+        player.call_deferred("die")
